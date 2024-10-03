782c6337976697ecf134a8d7fa29a0f330c6eac2
Klimanova Evgenia <mgenya22@gmail.com>
2024-10-03 17:16:59 +0300

    Add rules game

--- src/test/java/FieldTest.java
+++ src/test/java/FieldTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class FieldTest {
     static final String VERTICAL_ORIENTAL = "1";
     static final int HORIZONTAL_ORIENTAL = 2;
@@ -26,6 +29,28 @@ class FieldTest {
         declaredField.setAccessible(true);
 
         Assertions.assertThat(declaredField.get(field)).isEqualTo(ships);
+    }
+
+    @Test
+    void addShipOutsideField() {
+        List<Ship> ships = new ArrayList<>();
+
+        Coordinate startShipPosition = new Coordinate(9, 1);
+        Orientation oriental = Orientation.HORIZONTAL;
+        int sizeShip = 4;
+
+        Ship ship = new Ship(startShipPosition, oriental, sizeShip);
+        ships.add(ship);
+
+        Field field = new Field();
+
+        GameExeption exception = assertThrows(GameExeption.class, () -> {
+
+            field.addShip(startShipPosition, oriental, sizeShip);
+        });
+
+        String expected = "Корабль выходит за границы поля";
+        assertTrue(exception.getMessage().contains(expected));
     }
 
     @Test
